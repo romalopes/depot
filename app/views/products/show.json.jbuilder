78c49d5e43cd37f90268c3643d86e582de8161f6
romalopes <romalopes@yahoo.com.br>
2013-12-20 12:10:28 +1100

    Depot Scaffold

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :description, :image_url, :price, :date_creation, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '3d0f7676d8c059d8b5c06001f06ceb2ee388f44c38836d43b47a69e4b1bb557bfd3dd8f94a00a99bf7c5eb479ec67c2e1a3f818dd152ef5787e0c85d1c46b955'
